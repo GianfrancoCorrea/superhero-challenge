9604a7922f9617269a12002109f077ad8c7fe952
gian <gianfranco.jsx@gmail.com>
2023-02-27 16:23:17 -0300

    init

--- src/state/store.js
+++ src/state/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import superheroReducer from '../reducers/superhero/superheroSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    superhero: superheroReducer,
   },
 });
