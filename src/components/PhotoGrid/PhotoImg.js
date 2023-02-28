da9321fa630a18932f9e7519fc5d33a0aaaca292
gian <gianfranco.jsx@gmail.com>
2023-02-28 12:24:59 -0300

    loading state on img

--- src/components/PhotoGrid/PhotoImg.js
+++ src/components/PhotoGrid/PhotoImg.js
@@ -1,12 +1,26 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 function PhotoImg({ alt, images }) {
+    const [loaded, setLoaded] = useState(false);
+
     return (
-        <img
-            src={images.sm}
-            alt={alt}
-            className="w-full rounded-xl shadow-sm shadow-black"
-        />
+        <>
+            { loaded ? null
+                : (
+                    <div className="loading-pulse border shadow rounded-lg p-4 max-w-sm w-full mx-auto">
+                        <div className="animate-pulse flex space-x-4 place-content-center ">
+                            <div className="rounded-full bg-slate-700 h-10 w-10" />
+                        </div>
+                    </div>
+                )}
+            <img
+                src={images.sm}
+                alt={alt}
+                className="w-full rounded-xl shadow-sm shadow-black"
+                onLoad={() => setLoaded(true)}
+            />
+        </>
     );
 }
 
