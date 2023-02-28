889416ec59a5efb8634a9bb101d33553f8559de9
gian <gianfranco.jsx@gmail.com>
2023-02-28 12:47:31 -0300

    PhotoInfo with gradient bg

--- src/components/PhotoGrid/PhotoInfo.js
+++ src/components/PhotoGrid/PhotoInfo.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 function PhotoInfo({ item }) {
     return (
-        <div className="photo-info absolute bottom-0 text-left  bg-slate-800/[.35] p-2 w-full rounded-b-lg">
+        <div className="photo-info absolute bottom-0 text-left bg-gradient-to-b from-transparent to-slate-800 p-2 w-full rounded-b-lg">
             <h2 className="font-extrabold mb-1 drop-shadow-lg">{item.name}</h2>
 
             <div className="font-light drop-shadow-lg">
